202d74a6ac5bc5761fda7f13eca536f720738c3b
MarioSalamanca10 <jomasago10@gmail.com>
2026-05-21 02:19:34 -0500

    asd

--- server.js
+++ server.js
@@ -210,7 +210,8 @@ async function startServer() {
 
   const app = express();
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   app.get('/', (req, res) => {
     res.send('API de productos funcionando. Usa /api/products');
